34ea214ea6fbebd2205b6e3e8aa8742fa4d3875e
edugularte <edu.gularte@gmail.com>
2025-11-26 05:30:10 -0300

    Create ContatoPessoal.java

---
+++ src/br/com/agenda/model/ContatoPessoal.java
@@ -0,0 +1,32 @@
+package br.com.agenda.model;
+
+/**
+ * HERANÇA: ContatoPessoal herda de Contato (é-um Contato)
+ * Adiciona atributos específicos de contatos pessoais
+ */
+public class ContatoPessoal extends Contato { // HERANÇA: extends Contato
+    // ATRIBUTO ESPECÍFICO: dataAniversario (específico de contatos pessoais)
+    private String dataAniversario;
+
+    // CONSTRUTOR: Chama construtor da classe pai e inicializa atributo específico
+    public ContatoPessoal(String nome, String dataAniversario) {
+        super(nome); // Chama construtor da classe pai
+        this.dataAniversario = dataAniversario;
+    }
+
+    // ENCAPSULAMENTO: Getter e Setter específico
+    public String getDataAniversario() { return dataAniversario; }
+    public void setDataAniversario(String dataAniversario) { 
+        this.dataAniversario = dataAniversario; 
+    }
+
+    /**
+     * POLIMORFISMO: Sobrescrita do método toString()
+     * Reutiliza o comportamento da classe pai e adiciona comportamento específico
+     */
+    @Override
+    public String toString() {
+        return super.toString() + "Aniversário: " + dataAniversario + "\n" +
+               "Tipo: Contato Pessoal\n";
+    }
+}
