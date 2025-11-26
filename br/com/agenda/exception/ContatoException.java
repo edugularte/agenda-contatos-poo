aafe17832663d4dbf34b32cdbf2ade6e5ee8ef27
edugularte <edu.gularte@gmail.com>
2025-11-26 04:58:08 -0300

    Update ContatoException.java

--- br/com/agenda/exception/ContatoException.java
+++ br/com/agenda/exception/ContatoException.java
@@ -1,11 +1,18 @@
 package br.com.agenda.exception;
 
-public class ContatoException extends Exception {
-    public ContatoException(String message) {
-        super(message);
-    }
+/**
+ * TRATAMENTO DE EXCEÇÕES: Exceção personalizada para erros específicos da agenda
+ * Herda de Exception para criar uma exceção verificada
+ */
+public class ContatoException extends Exception { // HERANÇA para exceções
     
+    // CONSTRUTOR: Inicializa a exceção com mensagem de erro
+    public ContatoException(String message) {
+        super(message); // Chama construtor da classe pai (Exception)
+    }
+
+    // CONSTRUTOR: Inicializa com mensagem e causa
     public ContatoException(String message, Throwable cause) {
-        super(message, cause);
+        super(message, cause); // Chama construtor da classe pai
     }
 }
