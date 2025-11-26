b48764f6f040dd877435012caccaea568d4c4d85
edugularte <edu.gularte@gmail.com>
2025-11-26 04:39:33 -0300

    Create ContatoProfissional.java

---
+++ br/com/agenda/model/ContatoProfissional.java
@@ -0,0 +1,25 @@
+package br.com.agenda.model;
+
+public class ContatoProfissional extends Contato {
+    private String empresa;
+    private String cargo;
+    
+    public ContatoProfissional(String nome, String empresa, String cargo) {
+        super(nome);
+        this.empresa = empresa;
+        this.cargo = cargo;
+    }
+    
+    public String getEmpresa() { return empresa; }
+    public void setEmpresa(String empresa) { this.empresa = empresa; }
+    
+    public String getCargo() { return cargo; }
+    public void setCargo(String cargo) { this.cargo = cargo; }
+    
+    @Override
+    public String toString() {
+        return super.toString() + "Empresa: " + empresa + "\n" +
+               "Cargo: " + cargo + "\n" +
+               "Tipo: Contato Profissional\n";
+    }
+}
